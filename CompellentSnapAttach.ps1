3911fd3582cd52a84119c3397eaa4ad57a061053
bobalob <bobalob@gmail.com>
2016-09-21 13:25:24 +0100

    Initial Commit

---
+++ CompellentSnapAttach.ps1
@@ -0,0 +1,51 @@
+﻿Param(
+    $Credential = (Get-Credential),
+    [Parameter(Mandatory=$true)][string] $scSn,
+    [Parameter(Mandatory=$true)][string] $serverName,
+    [Parameter(Mandatory=$true)][string] $port,
+    [Parameter(Mandatory=$true)][string] $TargetVolume
+)
+
+#Load the module and connect to the controller
+. C:\Scripts\ConnectDellCompellent.PS1 -Credential $Credential -scSn $scSn -port $port
+if ($sc -eq $null) {break}
+
+#Check the replay doesn't already exist
+$ReplayView = Get-DellScVolume -Connection $conn | ? {$_.Name -eq "Automated View of $($TargetVolume)"}
+if ($ReplayView) {Write-Host "Error: Automated replay already exists for this volume, Break" -ForegroundColor Red ; Break }
+
+#Get the most recent replay for the volume
+$Replays = Get-DellScReplay -StorageCenter $sc -Connection $conn
+$ReplayTarget = $Replays | ? {$_.CreateVolume -match $TargetVolume} | Sort -Property ExpireTime | Select -Last 1
+
+Write-Host "Newest Replay for volume " -NoNewline
+Write-Host "$($TargetVolume)" -NoNewline -ForegroundColor Cyan
+Write-Host " : Expires " -NoNewline
+Write-Host $($ReplayTarget.ExpireTime).ToString("yyyy-MM-dd hh:mm") -ForegroundColor Cyan
+
+#Create and map a view of the replay to the target server
+Write-Host "Create Replay View..."
+$ReplayView = New-DellScReplayView -Instance $ReplayTarget -Name "Automated View of $($TargetVolume)" -Connection $conn -ReplayProfileList $Null
+$ScServer = Get-DellScServer -Connection $conn | ? {$_.Name -match $serverName}
+$VolMapping = Add-DellScVolumeToServerMap -Instance $ReplayView -Server $ScServer -Connection $conn
+
+#Rescan Disks
+Update-HostStorageCache
+
+#Post Attach Checks
+$MountedDisk = Get-Disk | ? {$_.UniqueId -eq $ReplayView.DeviceId}
+if ($MountedDisk) {
+  if ($MountedDisk.IsOffline) {
+    Set-Disk -UniqueId $MountedDisk.UniqueId -IsOffline $false
+    $MountedDisk = Get-Disk | ? {$_.UniqueId -eq $ReplayView.DeviceId}
+    if ($MountedDisk.IsOffline) {
+      Write-Host "Successfully connected the disk, but failed to bring online"
+    } else {
+      Write-Host "Successfully mounted the disk"
+    }
+  } else {
+    Write-Host "Successfully mounted the disk"
+  }
+} else {
+  Write-Host "Failed to mount the disk"
+}
---
+++ CompellentSnapDetach.ps1
@@ -0,0 +1,42 @@
+﻿Param(
+    $Credential = (Get-Credential),
+    [Parameter(Mandatory=$true)][string] $scSn,
+    [Parameter(Mandatory=$true)][string] $serverName,
+    [Parameter(Mandatory=$true)][string] $port,
+    [Parameter(Mandatory=$true)][string] $TargetVolume
+)
+
+#Load the module and connect to the controller
+. C:\Scripts\ConnectDellCompellent.PS1 -Credential $Credential -scSn $scSn -port $port
+if ($sc -eq $null) {break}
+
+#Get local disk object and SAN replay view
+$ReplayView = Get-DellScVolume -Connection $conn | ? {$_.Name -eq "Automated View of $($TargetVolume)"}
+$LocalDisk = Get-Disk | ? {$_.UniqueId -eq $ReplayView.DeviceId}
+
+#Offline local disk
+Write-Host "Off-lining and detching disk $($LocalDisk.Number)"
+$LocalDisk
+Set-Disk -UniqueId $LocalDisk.UniqueId -IsOffline $True
+$LocalDisk = Get-Disk | ? {$_.UniqueId -eq $ReplayView.DeviceId}
+
+#Post Detach Checks
+if ($LocalDisk.IsOffline) {
+  #Delete SAN replay view
+  $Removed = Remove-DellScVolume -Instance $ReplayView -Connection $conn -Confirm:$False
+  #Rescan Disks
+  Update-HostStorageCache
+
+  if (Get-Disk | ? {$_.UniqueId -eq $ReplayView.DeviceId}) {
+    Write-Host "Failed to remove the disk from this server"
+  } else {
+    $ReplayView = Get-DellScVolume -Connection $conn | ? {$_.Name -eq "Automated View of $($TargetVolume)"}
+    if ($ReplayView) {
+      Write-Host "Failed to remove the LUN from Dell SC"
+    } else {
+      Write-Host "Successfully Removed the Disk"
+    }
+  }
+} else {
+  Write-Host "Failed to offline the disk"
+}
